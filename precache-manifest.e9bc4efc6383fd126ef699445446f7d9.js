be220acaad1b95569c34efc55169f87266d5bd11
DinaSem <neispravimayas1@gmail.com>
2022-08-23 18:26:09 +0300

    Updates

---
+++ precache-manifest.e9bc4efc6383fd126ef699445446f7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fb83db05c78e3f04f693baa42381e1",
+    "url": "/Todolists/index.html"
+  },
+  {
+    "revision": "d8bbe4f3ba17afa213c0",
+    "url": "/Todolists/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/Todolists/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolists/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bbe4f3ba17afa213c0",
+    "url": "/Todolists/static/js/main.b80429de.chunk.js"
+  },
+  {
+    "revision": "2bbe966b1a7266edfd93",
+    "url": "/Todolists/static/js/runtime-main.a5ba1ef0.js"
+  }
+]);
